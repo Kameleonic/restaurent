3acbccac85e327f52367db2c206b3948470c3787
Kameleonic <959media@gmail.com>
2022-08-18 22:06:09 +0100

    changed register page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,7 +53,7 @@ https://templatemo.com/tm-558-klassy-cafe
                 <div class="col-12">
                     <nav class="main-nav">
                         <!-- ***** Logo Start ***** -->
-                        <a href="index.html" class="mt-4 logo">
+                        <a href="index.html" class="logo">
                             <img src="assets/images/klassy-logo.png" align="klassy cafe html template">
                         </a>
                         <!-- ***** Logo End ***** -->
