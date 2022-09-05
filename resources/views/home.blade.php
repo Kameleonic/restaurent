07d361bf2e819ac11554ec0c29f0fa51f55e3bee
KameleonicDNB <79947831+Kameleonic@users.noreply.github.com>
2022-09-05 16:45:47 +0100

    Added reservation success message.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -652,10 +652,37 @@ https://templatemo.com/tm-558-klassy-cafe
         });
     </script>
     <script>
-        Toastify({
-            text: "This is a toast",
-            duration: 3000
-        }).showToast();
+        @if (Session::has('message'))
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true
+            }
+            toastr.success("{{ session('message') }}");
+        @endif
+
+        @if (Session::has('error'))
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true
+            }
+            toastr.error("{{ session('error') }}");
+        @endif
+
+        @if (Session::has('info'))
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true
+            }
+            toastr.info("{{ session('info') }}");
+        @endif
+
+        @if (Session::has('warning'))
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true
+            }
+            toastr.warning("{{ session('warning') }}");
+        @endif
     </script>
 </body>
 
