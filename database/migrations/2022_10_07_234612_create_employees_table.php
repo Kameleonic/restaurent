bd960c4b70fd9c5561c372920e6aaf0e22d6faeb
Kameleonic <79947831+Kameleonic@users.noreply.github.com>
2022-10-08 00:54:14 +0100

    created employee migration model and controller

---
+++ database/migrations/2022_10_07_234612_create_employees_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id()->primaryKey();
+            $table->int('employee_id')->default('NULL');
+            $table->string('employee_type')->defualt('0');
+            $table->string('first_name');
+            $table->string('surname');
+            $table->text('address');
+            $table->string('email');
+            $table->string('tel_no');
+            $table->int('salary');
+            $table->int('contracted_hours');
+            $table->date('date_employed');
+            $table->int('employee_warning');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
